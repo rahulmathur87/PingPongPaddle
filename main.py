01acf6d40db5b23d0c2bb49393137e9f91944295
Rahul <rahulmathur87@gmail.com>
2025-10-18 16:45:06 +0530

    created ball

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+
 
 # Screen setup
 screen = Screen()
@@ -11,6 +13,7 @@ screen.tracer(0)
 # Importing paddles
 left_paddle = Paddle(-385)
 right_paddle = Paddle(378)
+ball = Ball()
 screen.update()
 
 screen.exitonclick()
--- paddle.py
+++ paddle.py
@@ -5,9 +5,10 @@ Y_COORDINATES = [-40, -20, 0, 20, 40]
 
 class Paddle:
     def __init__(self, x_coordinate):
+        body = []
         for coordinate in Y_COORDINATES:
             segment = Turtle("square")
             segment.color("white")
             segment.penup()
             segment.goto(x_coordinate, coordinate)
-
+            body.append(segment)
