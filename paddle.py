60cc22b30198946ae7043cbea6a87f891e6311a9
Rahul <rahulmathur87@gmail.com>
2025-10-19 04:15:47 +0530

    converted paddles into a single turtle instead of multiple turtle segments.

--- paddle.py
+++ paddle.py
@@ -1,22 +1,16 @@
 from turtle import Turtle
 
-Y_COORDINATES = [-40, -20, 0, 20, 40]
-
 
 class Paddle:
     def __init__(self, x_coordinate):
-        self.body = []
-        for coordinate in Y_COORDINATES:
-            segment = Turtle("square")
-            segment.color("white")
-            segment.penup()
-            segment.teleport(x_coordinate, coordinate)
-            self.body.append(segment)
+        self.body = Turtle("square")
+        self.body.color("white")
+        self.body.penup()
+        self.body.shapesize(stretch_wid=5, stretch_len=1)
+        self.body.teleport(x_coordinate, 0)
 
     def up(self):
-        for segment in self.body:
-            segment.goto(segment.xcor(), segment.ycor() + 20)
+        self.body.goto(self.body.xcor(), self.body.ycor() + 20)
 
     def down(self):
-        for segment in self.body:
-            segment.goto(segment.xcor(), segment.ycor() - 20)
+        self.body.goto(self.body.xcor(), self.body.ycor() - 20)
