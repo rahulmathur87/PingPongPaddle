e93db943248035199432f26b651f971e52fe8809
Rahul <rahulmathur87@gmail.com>
2025-10-18 16:45:06 +0530

    created ball

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
+
 
 # Screen setup
 screen = Screen()
@@ -8,9 +10,10 @@ screen.bgcolor("black")
 screen.setup(800, 600)
 screen.tracer(0)
 
-# Importing paddles
+# Importing paddles and ball
 left_paddle = Paddle(-385)
 right_paddle = Paddle(378)
+ball = Ball()
 screen.update()
 
 screen.exitonclick()
